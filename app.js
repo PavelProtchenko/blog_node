f4401ce142cacecd3811299672a8ff04e5308e66
PavelProtchenko <protchenkopa@gmail.com>
2019-01-31 16:32:23 +0300

    add post model to app

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const Post = require('./models/post');
+
 const app = express();
 
 //const data = { name: 'Burger',
@@ -9,13 +11,26 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const arr = ['hello', 'world', 'test']
+//const arr = ['hello', 'world', 'test']
 
-app.get('/', (req,res) => res.render('index', {arr: arr}));
+
+app.get('/', (req,res) => {
+  Post.find({}).then(posts => {
+    res.render('index', { posts: posts });
+  })
+});
+
+//res.render('index', {arr: arr}));
 
 app.get('/create', (req,res) => res.render('create'));
 app.post('/create', (req,res) => {
-  arr.push(req.body.text);
+  const { title, body } = req.body;
+
+  Post.create({
+    title: title,
+    body: body
+  }).then(post => console.log(post.id));
+
   res.redirect('/');
   console.log(arr);
 });
