e88da842ed98b4054d0787885245c3dd7da6c7f1
PavelProtchenko <protchenkopa@gmail.com>
2019-01-31 21:07:33 +0300

    add layout

--- app.js
+++ app.js
@@ -1,38 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
-const Post = require('./models/post');
+const path = require('path');
 
 const app = express();
 
-//const data = { name: 'Burger',
-//               age: 23 };
-
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
-
-//const arr = ['hello', 'world', 'test']
-
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  '/javascripts',
+  express.static(path.join(__dirname, 'node_modules', 'jquery', 'dist'))
+);
 
 app.get('/', (req,res) => {
-  Post.find({}).then(posts => {
-    res.render('index', { posts: posts });
-  })
-});
-
-//res.render('index', {arr: arr}));
-
-app.get('/create', (req,res) => res.render('create'));
-app.post('/create', (req,res) => {
-  const { title, body } = req.body;
-
-  Post.create({
-    title: title,
-    body: body
-  }).then(post => console.log(post.id));
-
-  res.redirect('/');
-  console.log(arr);
+  res.render('index');
 });
 
 module.exports = app;
