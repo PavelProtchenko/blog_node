8f9c3227289d4de7ac94fd5b6d396b3ba24a7398
PavelProtchenko <protchenkopa@gmail.com>
2019-02-10 13:43:42 +0300

    404 process and refactor a little bit

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const staticAsset = require('static-asset');
+const config = require('./config');
 
+// express
 const app = express();
 
+// sets and uses
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(staticAsset(path.join(__dirname, 'public')))
@@ -14,8 +17,26 @@ app.use(
   express.static(path.join(__dirname, 'node_modules', 'jquery', 'dist'))
 );
 
+// router
 app.get('/', (req,res) => {
   res.render('index');
 });
 
+// catch 404
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
 module.exports = app;
+
+// error handler
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.render('error', {
+    message: error.message,
+    error: !config.IS_PRODUCTION ? error : {},
+    title: 'Ooops...'
+  });
+});
